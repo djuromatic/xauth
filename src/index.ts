624867820a8c567da17077fad11db9d8773b7606
djuromatic <djuromatic991@gmail.com>
2023-04-24 15:01:23 +0200

    aws sns client

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import * as dotenv from 'dotenv';
 import { createServer } from './server.js';
+import { SesService } from './utils/aws/ses.js';
 const init = async () => {
   await dotenv.config();
 };
 
 await init();
 await createServer();
+
+const email = new SesService();
+email.sendTextEmail(['djuro.matic@mvpworkshop.co', 'djuromatic991@gmail.com'], 'Test', 'Test');
