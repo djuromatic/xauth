8bc8f31a57b8d34de9b1ff20d19297b3605ea893
djuromatic <djuromatic991@gmail.com>
2023-04-24 16:37:38 +0200

    removing temp example

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,8 @@
 import * as dotenv from 'dotenv';
 import { createServer } from './server.js';
-import { SesService } from './utils/aws/ses.js';
 const init = async () => {
   await dotenv.config();
 };
 
 await init();
 await createServer();
-
-const email = new SesService();
-email.sendTextEmail(['djuromatic991@gmail.com'], 'Test', 'Test');
