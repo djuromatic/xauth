710d95bb3e2ed25d940a9421190348b8aa99dc82
Ubuntu <ubuntu@ip-10-0-1-183.eu-central-1.compute.internal>
2023-05-18 13:35:39 +0000

    add redirect

--- src/helpers/provider-clients.ts
+++ src/helpers/provider-clients.ts
@@ -7,7 +7,7 @@ function getProviderClients(): ClientMetadata[] {
       client_secret: 'xauth',
       grant_types: ['authorization_code'],
       scope: 'openid profile email',
-      redirect_uris: ['https://xauth.test:6001'],
+      redirect_uris: ['https://xauth.test:6001', 'https://app.xauth.mvpworkshop.co'],
       response_types: ['code'],
       token_endpoint_auth_method: 'none'
     }
